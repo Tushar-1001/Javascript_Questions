9dd020db00ac120c3a8e95cdc754646f74645a5e
Tushar-1001 <sainitushar51@gmail.com>
2022-02-03 20:24:13 +0530

    JS

---
+++ sameLang.js
@@ -0,0 +1,13 @@
+function isSameLanguage(list) {
+    const lang = list[0].language;
+    return list.every(v => v.language === lang);
+  }
+
+
+ 
+
+  console.log(isSameLanguage([
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  ]))
